c4fa963fb4a19cab3a8ff236be0a9d84b4b9cf23
Nicholas Bova <bovan@USELESS-TRON5000>
2017-03-10 18:03:37 -0500

    now binds port on the client

--- client/ftpc.py
+++ client/ftpc.py
@@ -9,13 +9,14 @@ remote_ip = sys.argv[1] #first arg = remote-IP-on-gamma
 remote_port = sys.argv[2] #second arg = remote-port-on-gamma
 troll_port = sys.argv[3] #third arg = troll port on beta
 strfile = sys.argv[4] #fourth arg = file name
+local_ip = socket.gethostbyname(socket.gethostname()) #gets public ip of current server
 start_i = 0
 end_i = 1000
 size_buffer = 1000
 
 #creating the client here
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.bind(('localhost', 4000))
+client.bind((local_ip, 4000))
 
 #I should probably use the copy.py code from the last lab for this shouldn't I?
 
@@ -30,14 +31,14 @@ if os.path.isfile(strfile): #ensures file named "strfile" exists in current dire
 		#print("File size is: " + str(file_size)) #debug code
 
 		size_struct = struct.pack("i", file_size) #packing file_size for transfer to server
-		client.sendto(size_struct, ('localhost', int(troll_port)))#send the file size first to the troll on troll_port
+		client.sendto(size_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
 		data = client.recv(size_buffer) #wait to get data sent back from server for confirmation
-		client.sendto(strfile.encode(), ('localhost', int(troll_port)))#send the file name second
+		client.sendto(strfile.encode(), (local_ip, int(troll_port)))#send the file name second
 		data = client.recv(size_buffer)
 		while start_i < file_size: #seek from bin_file until it reaches the end
 			bin_file.seek(start_i) #1000 bytes starting at start_i
 			data = bin_file.read(end_i - start_i)
-			client.sendto(data, ('localhost', int(troll_port)))
+			client.sendto(data, (local_ip, int(troll_port)))
 			#copy_file.write(data) #writes 1000 bytes of data to copy_file 
 			start_i += size_buffer #increments start_i to move accross bin_file
 			end_i += size_buffer
