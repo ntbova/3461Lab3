9f07cbe901c7f8a6c64d403e654380a5e394c1f6
Nicholas Bova <bovan@USELESS-TRON5000>
2017-03-10 16:47:09 -0500

    first test

--- client/ftpc.py
+++ client/ftpc.py
@@ -7,7 +7,8 @@ import struct
 #Program global variables below
 remote_ip = sys.argv[1] #first arg = remote-IP-on-gamma
 remote_port = sys.argv[2] #second arg = remote-port-on-gamma
-strfile = sys.argv[3] #third arg = file name
+troll_port = sys.argv[3] #third arg = troll port on beta
+strfile = sys.argv[4] #fourth arg = file name
 start_i = 0
 end_i = 1000
 size_buffer = 1000
@@ -19,22 +20,22 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #sending the file here
 if os.path.isfile(strfile): #ensures file named "strfile" exists in current directory
-	print("File valid. Connecting to server...")
+	print("File valid. Connecting to troll...")
 	file_size = os.path.getsize(strfile)	
-	client.connect((remote_ip, int(remote_port))) #int to convert to integer
+	#client.connect((remote_ip, int(remote_port))) #int to convert to integer
 	with open(strfile, 'br') as bin_file: #opens pointer of file of binary reader as bin_file
 		#first thing to send is the file size
 		
 		#print("File size is: " + str(file_size)) #debug code
 		size_struct = struct.pack("i", file_size) #packing file_size for transfer to server
-		client.send(size_struct)#send the file size first
+		client.sendto(size_struct, ('localhost', troll_port))#send the file size first to the troll on troll_port
 		data = client.recv(size_buffer) #wait to get data sent back from server for confirmation
-		client.send(strfile.encode())#send the file name second
+		client.sendto(strfile.encode(), ('localhost', troll_port))#send the file name second
 		data = client.recv(size_buffer)
 		while start_i < file_size: #seek from bin_file until it reaches the end
 			bin_file.seek(start_i) #1000 bytes starting at start_i
 			data = bin_file.read(end_i - start_i)
-			client.sendall(data)
+			client.sendto(data, ('localhost', troll_port))
 			#copy_file.write(data) #writes 1000 bytes of data to copy_file 
 			start_i += size_buffer #increments start_i to move accross bin_file
 			end_i += size_buffer
--- server/ftps.py
+++ server/ftps.py
@@ -18,32 +18,26 @@ server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((tcp_ip, int(tcp_port)))
 
 #wait for the connection
-print ('\nWaiting for client to connect.')
-server.listen(1)
-conn, addr = server.accept()
-#print ('\nConnected by: '), addr
+print ('\nWaiting for packets to be sent...')
+# server.listen(1)
+# conn, addr = server.accept()
+# print ('\nConnected by: '), addr
 
-#loop until it no longer is recieving any data
-#while 1:
-data = conn.recv(4)
+# loop until it no longer is recieving any data
+# while 1:
+data, addr = conn.recvfrom(4)
 file_size = struct.unpack("i", data)
-conn.send(data)
-#if not data: break
-#check what kind of data it is by the size
-#if sys.getsizeof(data) == 4:
-#if sys.getsizeof(data) == 20:
-#else:
-#file_size = int(data.decode())
 data = conn.recv(20) #recieving file name
-conn.send(data) #send back to confirm
 file_name = data.decode()
 
 print("File size is: " + str(file_size)) #debug code
 
 with open(file_name, 'bw') as server_file:
-	while data: #reads until it is no longer recieving data
+	while 1: #reads until it is no longer recieving data
 			
 		data = conn.recv(size_buffer)
+		if not data:
+			break
 		#copy_file.write(data) #writes 1000 bytes of data to copy_file 			
 		server_file.write(data)
 		start_i += size_buffer #increments start_i to move accross bin_file
