8946293cec44fb64d7db98cd43b9308acefbe3f1
Nicholas Bova <bovan@USELESS-TRON5000>
2017-03-10 16:02:02 -0500

    reusing lab2 files

--- client/ftpc.py
+++ client/ftpc.py
@@ -1,6 +1,8 @@
+#Lab 2 - ftpc.py - Nicholas Bova
 import socket
 import sys
 import os
+import struct
 
 #Program global variables below
 remote_ip = sys.argv[1] #first arg = remote-IP-on-gamma
@@ -17,25 +19,29 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #sending the file here
 if os.path.isfile(strfile): #ensures file named "strfile" exists in current directory
-	print("File valid. Connecting to server...")	
+	print("File valid. Connecting to server...")
+	file_size = os.path.getsize(strfile)	
 	client.connect((remote_ip, int(remote_port))) #int to convert to integer
 	with open(strfile, 'br') as bin_file: #opens pointer of file of binary reader as bin_file
 		#first thing to send is the file size
-		file_size = os.path.getsize(strfile)
-		client.sendall(file_size)#send the file size first
-		client.sendall(strfile)#send the file name second
-		while start_i < bin_size: #seek from bin_file until it reaches the end
+		
+		#print("File size is: " + str(file_size)) #debug code
+		size_struct = struct.pack("i", file_size) #packing file_size for transfer to server
+		client.send(size_struct)#send the file size first
+		data = client.recv(size_buffer) #wait to get data sent back from server for confirmation
+		client.send(strfile.encode())#send the file name second
+		data = client.recv(size_buffer)
+		while start_i < file_size: #seek from bin_file until it reaches the end
 			bin_file.seek(start_i) #1000 bytes starting at start_i
 			data = bin_file.read(end_i - start_i)
-
 			client.sendall(data)
 			#copy_file.write(data) #writes 1000 bytes of data to copy_file 
-
-
-			start_i += byte_size #increments start_i to move accross bin_file
-			end_i += byte_size
+			start_i += size_buffer #increments start_i to move accross bin_file
+			end_i += size_buffer
 		#clean-up after exiting loop
 		bin_file.close()
-		copy_file.close()
+		print("\nFile transfer complete!")
 else: #print error if file not found
 	print("File not found.")
+#clean-up
+client.close()
--- server/ftps.py
+++ server/ftps.py
@@ -1,47 +1,56 @@
+#Lab 2 - ftps.py - Nicholas Bova
 import socket
 import sys
 import os
+import struct
 
 #Program global variables below
 start_i = 0
 end_i = 1000
 size_buffer = 1000
 tcp_ip = '' #DNS name of where ftps.py is
-tcp_port = 5000 #ftp port num
+tcp_port = sys.argv[1] #first arg = port number
 file_size = 0 #to be set when size is recieved from the client
 file_name = '' #to bet set when file name is recieved from the client
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((tcp_ip, tcp_port))
+#create server socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((tcp_ip, int(tcp_port)))
 
 #wait for the connection
-print '\nWaiting for client to connect.'
+print ('\nWaiting for client to connect.')
 server.listen(1)
 conn, addr = server.accept()
-print '\nConnected by', addr
+#print ('\nConnected by: '), addr
 
 #loop until it no longer is recieving any data
-while 1:
-	data = conn.recv(size_buffer)	
-	if not data: break
-	#check what kind of data it is by the size
-	if sys.getsizeof(data) == 4:
-		file_size = data
-	if sys.getsizeof(data) == 20:
-		file_name = data
-	else:
-		with open(file_name, 'bw') as server_file:
-			while start_i < file_size: #seek from bin_file until it reaches the end
-			
-				data = conn.recv(size_buffer)
-				#copy_file.write(data) #writes 1000 bytes of data to copy_file 			
-				server_file.write(data)
+#while 1:
+data = conn.recv(4)
+file_size = struct.unpack("i", data)
+conn.send(data)
+#if not data: break
+#check what kind of data it is by the size
+#if sys.getsizeof(data) == 4:
+#if sys.getsizeof(data) == 20:
+#else:
+#file_size = int(data.decode())
+data = conn.recv(20) #recieving file name
+conn.send(data) #send back to confirm
+file_name = data.decode()
 
-				start_i += byte_size #increments start_i to move accross bin_file
-				end_i += byte_size
-		
-	
-	conn.sendall(data)
+print("File size is: " + str(file_size)) #debug code
+
+with open(file_name, 'bw') as server_file:
+	while data: #reads until it is no longer recieving data
+			
+		data = conn.recv(size_buffer)
+		#copy_file.write(data) #writes 1000 bytes of data to copy_file 			
+		server_file.write(data)
+		start_i += size_buffer #increments start_i to move accross bin_file
+		end_i += size_buffer	
+	server_file.close()
+conn.sendall(data)
 #clean up
+print("\nTransfer Complete!")
 server.close()
 conn.close()
