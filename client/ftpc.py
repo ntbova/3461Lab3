ed82578f48c7413f36eea4eb17230efd1d14e84a
Nicholas Bova <bovan@USELESS-TRON5000>
2017-03-10 21:18:19 -0500

    server fix to wait for all UDP datagrams

--- client/ftpc.py
+++ client/ftpc.py
@@ -4,6 +4,7 @@ import time
 import sys
 import os
 import struct
+import netaddr
 
 #Program global variables below
 remote_ip = sys.argv[1] #first arg = remote-IP-on-gamma
@@ -21,7 +22,7 @@ size_buffer = 1000
 #creating the client here
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.bind((local_ip, 4000))
-s_local_ip = bytes(local_ip, 'utf-8') #bytes form string to pack into structs
+s_local_ip = int(netaddr.IPAddress(local_ip)) #bytes form string to pack into structs
 s_strfile = bytes(strfile, 'utf-8')
 
 #I should probably use the copy.py code from the last lab for this shouldn't I?
@@ -36,16 +37,16 @@ if os.path.isfile(strfile): #ensures file named "strfile" exists in current dire
 		
 		#print("File size is: " + str(file_size)) #debug code
 
-		size_struct = struct.pack("4shhi", s_local_ip, 4000, 1, file_size) #packing file_size for transfer to server
+		size_struct = struct.pack("Ihhi", s_local_ip, 4000, 1, file_size) #packing file_size for transfer to server
 		client.sendto(size_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
 		data = client.recv(size_buffer) #wait to get data sent back from server for confirmation
-		str_struct = struct.pack("4shh20s", s_local_ip, 4000, 2, strfile.encode())
+		str_struct = struct.pack("Ihh20s", s_local_ip, 4000, 2, strfile.encode())
 		client.sendto(str_struct, (local_ip, int(troll_port)))#send the file name second
 		data = client.recv(size_buffer)
 		while start_i < file_size: #seek from bin_file until it reaches the end
 			bin_file.seek(start_i) #1000 bytes starting at start_i
 			data = bin_file.read(end_i - start_i)
-			data_struct = struct.pack("4shh1000s", s_local_ip, 4000, 3, data)
+			data_struct = struct.pack("Ihh1000s", s_local_ip, 4000, 3, data)
 			client.sendto(data_struct, (local_ip, int(troll_port)))
 			#copy_file.write(data) #writes 1000 bytes of data to copy_file 
 			start_i += size_buffer #increments start_i to move accross bin_file
