23e7878f067c3af5799456188b0a776425ff5a9b
nicholas tyler bova <bova.26@zeta.cse.ohio-state.edu>
2017-04-18 22:14:46 -0400

    Let's do this

--- client/ftpc.py
+++ client/ftpc.py
@@ -4,11 +4,22 @@ import time
 import sys
 import os
 import struct
+import select
 
 #ip num converter
 def ip2l(ip):
     packedIP = inet_aton(ip)
     return struct.unpack("!L", packedIP)[0]
+	
+#checks the ACK_bit with data and resets ACK_bit accordingly
+
+def change_ACK(ack):
+	if ack == 0:
+		ack = 1
+	else:
+		ack = 0
+	return ack
+
 
 #Program global variables below
 remote_ip = sys.argv[1] #first arg = remote-IP-on-gamma
@@ -19,14 +30,18 @@ local_ip = gethostbyname(gethostname()) #gets public ip of current server
 start_i = 0
 end_i = 1000
 size_buffer = 1000
+ACK_bit = 0; #initial ACK bit
 #udp payload segment
 #payload = struct.pack('ihh1000s', remote_ip, remote_port, 1)
 
+
+
 #creating the client here
 client = socket(AF_INET, SOCK_DGRAM)
-client.bind((local_ip, 4000))
+client.bind((local_ip, 4000)) #CLIENT defaults to port 4000 here
 s_local_ip = ip2l(local_ip)#bytes form string to pack into structs
 s_strfile = bytes(strfile, 'utf-8')
+client.setblocking(0)
 
 #I should probably use the copy.py code from the last lab for this shouldn't I?
 
@@ -40,18 +55,61 @@ try:
 			#first thing to send is the file size
 			
 			#print("File size is: " + str(file_size)) #debug code
-			client.settimeout(2) #timesout if it no longer recieves data
+			#client.settimeout(2) #timesout if it no longer recieves data
 			size_struct = struct.pack("lhhi", s_local_ip, 4000, 1, file_size) #packing file_size for transfer to server
 			client.sendto(size_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
-			data = client.recv(size_buffer) #wait to get data sent back from server for confirmation
+			ready = select.select([client],[],[],0.05) #Timeout time of 2 seconds
+			r_ack = 0;
+			while 1:
+				if ready[0]:
+					data = client.recv(size_buffer)
+					r_ack = struct.unpack("i", data)
+					if r_ack[0] != ACK_bit:
+						client.sendto(size_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+						ready = select.select([client],[],[],0.05)
+					else:
+						ACK_bit = change_ACK(ACK_bit)
+						break
+				else:
+					client.sendto(size_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+					ready = select.select([client],[],[],0.05)
+			print(str(r_ack[0]))
 			str_struct = struct.pack("lhh20s", s_local_ip, 4000, 2, strfile.encode('utf-8', 'ignore'))
 			client.sendto(str_struct, (local_ip, int(troll_port)))#send the file name second
-			data = client.recv(size_buffer)
+			ready = select.select([client],[],[],2) 
+			while 1:
+				if ready[0]:
+					data = client.recv(size_buffer)
+					r_ack = struct.unpack("i", data)
+					if r_ack[0] != ACK_bit:
+						client.sendto(str_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+						ready = select.select([client],[],[],0.05)
+					else:
+						ACK_bit = change_ACK(ACK_bit)
+						break
+				else:
+					client.sendto(str_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+					ready = select.select([client],[],[],0.05)
 			while start_i < file_size: #seek from bin_file until it reaches the end
 				bin_file.seek(start_i) #1000 bytes starting at start_i
 				data = bin_file.read(end_i - start_i)
 				data_struct = struct.pack("lhh1000s", s_local_ip, 4000, 3, data)
 				client.sendto(data_struct, (local_ip, int(troll_port)))
+				ready = select.select([client],[],[],2) 
+				print(str(r_ack[0]))
+				while 1:
+					if ready[0]:
+						data = client.recv(size_buffer)
+						r_ack = struct.unpack("i", data)
+						if r_ack[0] != ACK_bit:
+							client.sendto(data_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+							ready = select.select([client],[],[],0.05)
+						else:
+							ACK_bit = change_ACK(ACK_bit)
+							break
+					else:
+						client.sendto(data_struct, (local_ip, int(troll_port)))#send the file size first to the troll on troll_port
+						ready = select.select([client],[],[],0.05)
 				#copy_file.write(data) #writes 1000 bytes of data to copy_file 
 				start_i += size_buffer #increments start_i to move accross bin_file
 				end_i += size_buffer
--- server/ftps.py
+++ server/ftps.py
@@ -1,18 +1,28 @@
 #Lab 3 - ftps.py - Nicholas Bova
 from socket import *
+import time
 import sys
-import select
 import os
 import struct
+import select
+
+def change_ACK(ack):
+	if ack == 0:
+		ack = 1
+	else:
+		ack = 0
+	return ack
 
 #Program global variables below
 start_i = 0
 end_i = 1000
 size_buffer = 1000
-tcp_ip = '' #DNS name of where ftps.py is
+tcp_ip = gethostbyname(gethostname()) #DNS name of where ftps.py is
 tcp_port = sys.argv[1] #first arg = port number
+troll_port = sys.argv[2] #second arg = troll port number
 file_size = 0 #to be set when size is recieved from the client
 file_name = '' #to bet set when file name is recieved from the client
+ACK_bit = 0; #initial ACK bit
 
 #create server socket
 server = socket(AF_INET, SOCK_DGRAM)
@@ -29,15 +39,35 @@ try:
 	while 1:
 		data, addr = server.recvfrom(size_buffer) #receiving file size
 		file_size = struct.unpack("lhhi", data)
-		server.sendto(data, addr) 
+		ack = struct.pack('i', ACK_bit)
+		ready = select.select([server],[],[],0.01) #Timeout time of 2 seconds
+		while 1:
+			if ready[0]:
+				data, addr = server.recvfrom(size_buffer)
+				break
+			else:
+				server.sendto(ack, (tcp_ip, int(troll_port)))
+				ready = select.select([server],[],[],0.01)				
+		server.sendto(data, (tcp_ip, int(troll_port)))
+		ACK_bit = change_ACK(ACK_bit)
 		break
 	#DEBUG CODE
 	print ('\nData is being received. Please wait.')
 	while 1:
-		server.settimeout(2) #timesout if it no longer recieves data
+		#server.settimeout(2) #timesout if it no longer recieves data
 		data, addr = server.recvfrom(size_buffer) #receiving file name
 		file_name = struct.unpack("lhh20s", data)
-		server.sendto(data, addr)
+		ack = struct.pack('i', ACK_bit)
+		ready = select.select([server],[],[],0.01) #Timeout time of 2 seconds
+		server.sendto(ack, (tcp_ip, int(troll_port)))
+		while 1:
+			if ready[0]:
+				data, addr = server.recvfrom(size_buffer)
+				break
+			else:
+				server.sendto(data, (tcp_ip, int(troll_port)))
+				ready = select.select([server],[],[],0.01)
+		ACK_bit = change_ACK(ACK_bit)
 		break
 	#DEBUG CODE
 	# print("File size is: " + str(file_size)) #debug code
@@ -45,9 +75,20 @@ try:
 	decode_name = decode_name.translate(dict.fromkeys(range(32))) #removes any null escape characters that can cause issues with open function
 	with open(decode_name, 'bw') as server_file:
 		while data: #reads until receives terminating str data
-			server.settimeout(2) #timesout if it no longer recieves data
+			server.settimeout(10) #timesout if it no longer recieves data
 			data, addr = server.recvfrom(size_buffer + 24)
 			file_part = struct.unpack("lhh1000s", data)
+			data = struct.pack('i', ACK_bit)
+			server.sendto(data, (tcp_ip, int(troll_port)))
+			ready = select.select([server],[],[],0.15) #Timeout time of 2 seconds
+			while 1:
+				if ready[0]:
+					data, addr = server.recvfrom(size_buffer)
+					break
+				else:
+					server.sendto(data, (tcp_ip, int(troll_port)))	
+					ready = select.select([server],[],[],0.01)
+			ACK_bit = change_ACK(ACK_bit)
 			#copy_file.write(data) #writes 1000 bytes of data to copy_file 			
 			server_file.write(file_part[3])
 			start_i += size_buffer #increments start_i to move accross bin_file
